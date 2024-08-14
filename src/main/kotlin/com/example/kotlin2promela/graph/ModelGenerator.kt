c8a11dadd63fa6f4bceda8d670c9d014298e62ed
Bob.Brockbernd <bobbrockbernd@gmail.com>
2024-08-14 16:17:55 +0200

    Topological sort types

--- src/main/kotlin/com/example/kotlin2promela/graph/ModelGenerator.kt
+++ src/main/kotlin/com/example/kotlin2promela/graph/ModelGenerator.kt
@@ -2,6 +2,7 @@ package com.example.kotlin2promela.graph
 
 import com.example.kotlin2promela.graph.action.ChannelInitDLAction
 import com.example.kotlin2promela.graph.variablePassing.variableTypes.DLStruct
+import java.util.*
 
 class ModelGenerator(private val dlGraph: DeadlockGraph) {
     
@@ -11,12 +12,18 @@ class ModelGenerator(private val dlGraph: DeadlockGraph) {
     fun generateForEntryPoint(fn: FunctionNode): String = buildString {   
         exploreFun(fn)
         collectChannels()
-        
-        funSet.filter {it.isConstructor} 
-            .forEach { 
-                appendLine("/* class ${it.info.fqName} */")
-                appendLine("typedef ${it.returnType.promType()} {")
-                (it.returnType as? DLStruct)?.propertyConsumers?.forEach { (name, consumer) ->
+        funSet
+            .filter { it.isConstructor }
+            .map { it.returnType }
+            .filterIsInstance<DLStruct>()
+            .topologicalSortedTypes { it.propertyConsumers.values
+                .map { tp -> tp.consumesFrom?.type!! }
+                .filterIsInstance<DLStruct>()
+            }
+            .forEach {
+                appendLine("/* class ${it.fqName} */")
+                appendLine("typedef ${it.promType()} {")
+                it.propertyConsumers.forEach { (name, consumer) ->
                     appendLine("    ${consumer.consumesFrom!!.type.promType()} $name")
                 }
                 appendLine("}")
@@ -50,7 +57,38 @@ class ModelGenerator(private val dlGraph: DeadlockGraph) {
         fn.getChildNodes().forEach { exploreFun(it) }
     }
     
+    class TypeDep<T>(val dlType: T) {
+        val children: MutableList<TypeDep<T>> = mutableListOf()
+        var parents: MutableList<TypeDep<T>> = mutableListOf()
+    }
+    
+    private inline fun <T> List<T>.topologicalSortedTypes(getChildren: (T) -> List<T> ): List<T> {
+        // construct type tree
+        val map: MutableMap<T, TypeDep<T>> = mutableMapOf()
+        this.forEach { currentType -> map[currentType] = TypeDep(currentType) }
+        this.forEach { currentType -> 
+            getChildren(currentType).forEach { childType -> 
+                    map[currentType]!!.children.add(map[childType]!!)
+                    map[childType]!!.parents.add(map[currentType]!!)
+                }
+        }
+        
+        val toVisit: MutableList<TypeDep<T>> = map.values.filter { it.parents.isEmpty() }.toMutableList()
+        val sorted: MutableList<TypeDep<T>> = mutableListOf()
+        
+        while (toVisit.isNotEmpty()) {
+            val current = toVisit.removeLast()
+            sorted.add(current)
+            current.children
+                .toList()
+                .forEach { child ->
+                    current.children.remove(child)
+                    child.parents.remove(current)
+                    if (child.parents.isEmpty()) toVisit.add(0, child)
+            }
+        }
+        return sorted.reversed().map { it.dlType }
+    }
+    
     private fun collectChannels() = chanSet.addAll(funSet.flatMap { it.getChanInits() })
-    
-    
 }
